d61f0ad1fde72fba67a64b950a926f037f50fe50
Garrett Stewart <roodymawnstr@gmail.com>
2016-02-11 12:04:22 -0600

    Initial commit

---
+++ cologne-1.0.js
@@ -0,0 +1,15 @@
+/*
+	Cologne.js - JavaScript that teaches you JavaScript
+	@Author: Garrett Stewart
+	@Version: 1.0
+	@Purpose: The literal purpose of Cologne.js is to make your JavaScript attractive.
+	In order to keep your JavaScript cool and from smelling bad, use deodorant.js. But,
+	that's underwhelming if you are trying to impress. Therefore, use Cologne.js to make
+	your JavaScript not just not smell bad, but make it smell great. Cologne.js actually just
+	teaches you how to write good JavaScript, pretty much by guiding you through how Cologne.js
+	itself would implement some really simple things. Mostly just repetitive MVC and class stuff.
+	Don't worry, spray some Cologne.js and your JavaScript will smell great.
+*/
+(function(global) {
+	
+})(typeof window !== "undefined" ? window : this);
